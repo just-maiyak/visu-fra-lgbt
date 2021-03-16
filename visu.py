a59bf157276b7803beb63fde274228023a9d45b7
Jean-Marc <jmarc.fares@icloud.com>
2021-03-16 20:24:15 +0000

    Update visu.py
    Update main app for serving on alonely.place,
    Add color styles

--- visu.py
+++ visu.py
@@ -5,9 +5,14 @@ import plotly.express as px
 
 import dataprep as prep
 
+# Styling
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(
+    __name__, url_base_pathname="/vfl/", external_stylesheets=external_stylesheets
+)
+colors = {"background": "#201f25", "text": "#f8f9f1"}
 
+# Figures declaration
 groups = prep.groups
 
 country_counts = prep.open_data()["counts"]
@@ -17,16 +22,29 @@ fig = px.bar(
     y="CountryName",
     x=groups,
 )
+fig.update_layout(
+    plot_bgcolor=colors["background"],
+    paper_bgcolor=colors["background"],
+    font_color=colors["text"],
+)
 
 app.layout = dhc.Div(
-    [
-        dhc.H1("EU-FRA LGBT Survey from 2021"),
+    style={"backgroundColor": colors["background"]},
+    children=[
+        dhc.H1(
+            "EU-FRA LGBT Survey from 2021",
+            style={"textAlign": "center", "color": colors["text"]},
+        ),
         dhc.Div("Subtitle"),
-        dcc.Graph(id="Polled countries repartition", figure=fig),
-    ]
+        dcc.Graph(
+            id="Polled countries repartition",
+            figure=fig,
+            style={"textAlign": "center", "color": colors["text"]},
+        ),
+    ],
 )
 
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=6969)
